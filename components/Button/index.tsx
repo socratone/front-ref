a17cbcf3f35cc3a911d714b96551af19774a6fb8
socratone <gim2origin@gmail.com>
2021-12-24 20:49:37 +0900

    feat: styled components 적용

---
+++ components/Button/index.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+type ButtonProps = {
+  onClick?: () => void;
+  children: string;
+};
+
+const Button = ({ onClick, children }: ButtonProps) => {
+  return <Container onClick={onClick}>{children}</Container>;
+};
+
+const Container = styled.button`
+  background: dodgerblue;
+  color: white;
+  font-weight: 600;
+  height: 40px;
+  border: 0;
+  border-radius: 5px;
+  cursor: pointer;
+
+  :hover {
+    background: #166fc9;
+  }
+
+  :active {
+    background: #0e559c;
+  }
+`;
+
+export default Button;
