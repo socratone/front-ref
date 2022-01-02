bbb97a06697842d47f9bbebe91efa7c49f0a25bd
John Kim <john@gymtinc.com>
2022-01-02 14:20:27 +0900

    fix: CircleIcon에 빠진 fill 속성 추가

--- components/BulletText/CircleIcon.tsx
+++ components/BulletText/CircleIcon.tsx
@@ -1,7 +1,11 @@
-const CircleIcon = () => {
+type CircleIconProps = {
+  fill?: string;
+};
+
+const CircleIcon = ({ fill }: CircleIconProps) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
-      <circle cx="9" cy="9" r="3" />
+      <circle cx="9" cy="9" r="3" fill={fill} />
     </svg>
   );
 };
